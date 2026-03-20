3aa3a6cbb3d7b154d90a0c792159f60e64c217b3
Roman <romansidorcuk1@gmail.com>
2026-03-21 00:37:02 +0300

    initial commit for authentification service

---
+++ authentificationservice/src/main/java/by/innowise/authentification_service/AuthentificationServiceApplication.java
@@ -0,0 +1,13 @@
+package by.innowise.authentification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthentificationServiceApplication.class, args);
+    }
+
+}
