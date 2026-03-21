f3bb8b67265cd9764a7bd075071107b4656870eb
Roman <romansidorcuk1@gmail.com>
2026-03-22 02:20:58 +0300

    add finbylogin method in repository

--- authentificationservice/src/main/java/by/innowise/authentificationservice/repository/CredentialsRepository.java
+++ authentificationservice/src/main/java/by/innowise/authentificationservice/repository/CredentialsRepository.java
@@ -4,6 +4,11 @@ import by.innowise.authentificationservice.entity.Credentials;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CredentialsRepository extends JpaRepository<Credentials, Long> {
+
+    Optional<Credentials> findByLogin(String login);
+
 }
