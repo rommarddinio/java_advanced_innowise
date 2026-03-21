07ab8049d3d53dba9d0023e635ced49e1aeb196c
Roman <romansidorcuk1@gmail.com>
2026-03-22 02:44:02 +0300

    add handling exceptions

---
+++ authenticationservice/src/main/java/by/innowise/authenticationservice/exception/handler/MyExceptionHandler.java
@@ -0,0 +1,52 @@
+package by.innowise.authenticationservice.exception.handler;
+
+import by.innowise.authenticationservice.exception.EmptyTokenException;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class MyExceptionHandler {
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<String> handleAuthentificationException(AuthenticationException e) {
+        return new ResponseEntity<>("Incorrect login or password", HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        return new ResponseEntity<>("Unique constraint was broken", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ResponseEntity<>("Illegal request argument", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<String> handleExpiredJwtException(ExpiredJwtException e) {
+        return new ResponseEntity<>("Token is expired", HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(SignatureException.class)
+    public ResponseEntity<String> handleSignatureException(SignatureException e) {
+        return new ResponseEntity<>("Token has wrong signature", HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(MalformedJwtException.class)
+    public ResponseEntity<String> handleMalformedJwtException(MalformedJwtException e) {
+        return new ResponseEntity<>("Token is broken", HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(EmptyTokenException.class)
+    public ResponseEntity<String> handleEmptyTokenException(EmptyTokenException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+}
