d50d46a070a3a3c5bfbad26ad8aaac14e40bc26b
Roman <romansidorcuk1@gmail.com>
2026-03-23 01:28:15 +0300

    [user-service] update exception handling

--- userservice/src/main/java/by/innowise/user_service/exception/handler/MyExceptionHandler.java
+++ userservice/src/main/java/by/innowise/user_service/exception/handler/MyExceptionHandler.java
@@ -2,10 +2,12 @@ package by.innowise.user_service.exception.handler;
 
 import by.innowise.user_service.exception.CardLimitException;
 import by.innowise.user_service.exception.CardNotFoundException;
+import by.innowise.user_service.exception.UnaccessibleCardException;
 import by.innowise.user_service.exception.UserNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -44,6 +46,16 @@ public class MyExceptionHandler {
                 .toList();
 
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException e) {
+        return new ResponseEntity<>("Access denied", HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(UnaccessibleCardException.class)
+    public ResponseEntity<String> handleUnaccessibleCardExceptions(UnaccessibleCardException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
     }
 
 }
