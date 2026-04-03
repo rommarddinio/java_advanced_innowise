bddb5936347baa7591cc56450ccfc8318f68225a
Roman <romansidorcuk1@gmail.com>
2026-04-03 04:25:09 +0300

    add specification

---
+++ orderservice/src/main/java/by/innowise/orderservice/specification/OrderSpecifications.java
@@ -0,0 +1,32 @@
+package by.innowise.orderservice.specification;
+
+import by.innowise.orderservice.entity.Order;
+import by.innowise.orderservice.enums.Status;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.time.Instant;
+
+public class OrderSpecifications {
+
+    public static Specification<Order> hasDate(Instant startDate, Instant endDate) {
+        return (((root, query, criteriaBuilder) -> {
+            if (startDate != null && endDate != null) {
+                return criteriaBuilder.between(root.get("createdAt"), startDate, endDate);
+            } else if (startDate != null) {
+                return criteriaBuilder.greaterThanOrEqualTo(root.get("createdAt"), startDate);
+            } else if (endDate != null) {
+                return criteriaBuilder.lessThanOrEqualTo(root.get("createdAt"), endDate);
+            } else {
+                return criteriaBuilder.conjunction();
+            }
+        }));
+    }
+
+    public static Specification<Order> hasStatus(Status status) {
+        return (((root, query, criteriaBuilder) ->
+                (status!=null) ?
+                        criteriaBuilder.equal(root.get("status"), status) :
+                        criteriaBuilder.conjunction()));
+    }
+
+}
